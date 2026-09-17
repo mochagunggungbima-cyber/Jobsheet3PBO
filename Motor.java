91b638cbd14b02fbb8dc995b2860118720cd5c48
mochagunggungbima-cyber <bimaagungbimzz@gmail.com>
2026-09-17 21:52:03 +0700

    Percobaan 2

--- Motor.java
+++ Motor.java
@@ -1,7 +1,7 @@
 public class Motor {
-   public String platNomor;
-   public boolean isMesinOn;
-   public int kecepatan;
+   private String platNomor;
+   private boolean isMesinOn;
+   private int kecepatan;
 
 
    public void displayStatus(){
@@ -18,6 +18,41 @@ public class Motor {
 
        System.out.println("Kecepatan:" + this.kecepatan);
        System.out.println("=========================");
+   }
+
+
+   public String getPlatNomor() {
+       return platNomor;
+   }
+
+
+   public void setPlatNomor(String platNomor) {
+       this.platNomor = platNomor;
+   }
+
+
+   public boolean isIsMesinOn() {
+       return isMesinOn;
+   }
+
+
+   public void setIsMesinOn(boolean isMesinOn) {
+       this.isMesinOn = isMesinOn;
+   }
+
+
+   public int getKecepatan() {
+       return kecepatan;
+   }
+
+
+   public void setKecepatan(int kecepatan) {
+       if (!this.isMesinOn && kecepatan > 0) {
+           System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+       }
+       else{
+           this.kecepatan = kecepatan;
+       }
    }
 }
 
