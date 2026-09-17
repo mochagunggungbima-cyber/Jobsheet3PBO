da29277823f475c0795b172bd8c991ca8124e861
mochagunggungbima-cyber <bimaagungbimzz@gmail.com>
2026-09-17 21:57:57 +0700

    Modifikasi Kode Pertanyaan No 4 Percobaan 2

--- Motor.java
+++ Motor.java
@@ -47,12 +47,13 @@ public class Motor {
 
 
    public void setKecepatan(int kecepatan) {
-       if (!this.isMesinOn && kecepatan > 0) {
-           System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
-       }
-       else{
-           this.kecepatan = kecepatan;
-       }
-   }
+        if (!this.isMesinOn && kecepatan > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else if (kecepatan > 100) {
+            System.out.println("Kecepatan tidak boleh lebih dari 100");
+        } else {
+            this.kecepatan = kecepatan;
+        }
+    }
 }
 
